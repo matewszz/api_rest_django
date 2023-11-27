0e49f1c99749783ceba8957b033e3beb4971e9d3
mateus <mferreira.nox@gmail.com>
2023-11-27 14:56:17 -0300

    update serializers

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,27 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
-class Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = { # O email não vai ser apresentado quando consultar a avaliação
+            'email': {'writhe_only': True}
+        }
+        model = Avaliacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+            )
+        
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
